12656c62bbc33f56e9661b89f37742e0044356a7
jamestang12 <homing.tang@ryerson.ca>
2020-03-23 19:29:46 -0400

    First Commit (display top 10 songs in the country)

---
+++ src/components/layout/Index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tracks from '../tracks/Tracks';
+
+
+const Index = () => {
+    return (
+        <React.Fragment>
+            <Tracks />
+        </React.Fragment>
+        
+        )
+}
+
+export default Index;
