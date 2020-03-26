40f5c3c98d2b5f2130dfce78897093b3d272bda2
jamestang12 <homing.tang@ryerson.ca>
2020-03-26 17:39:54 -0400

    Final Commit

--- src/context.js
+++ src/context.js
@@ -3,9 +3,23 @@ import axios from 'axios';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SEARCH_TRACKS':
+            return {
+                ...state,
+                track_list: action.payload,
+                heading: 'Search Results'
+            };
+        default:
+            return state;
+    }
+}
+
 export class Provider extends Component {
     state = {
-        track_list: [], heading: 'Top 10 Tracks'
+        track_list: [], heading: 'Top 10 Tracks',
+        dispatch: action => this.setState(state => reducer(state, action))
     };
 
     componentDidMount() {
